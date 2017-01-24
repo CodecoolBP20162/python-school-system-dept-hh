c476ed8051a98f0e4eeae808139dfd55cd07bc16
dry <szarazdenes@gmail.com>
2017-01-24 13:49:35 +0100

    Modular db connection implementation

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from peewee import *
 # Configure your database connection here
 # database name = should be your username on your laptop
 # database user = should be your username on your laptop
-db = PostgresqlDatabase('dbname', user='dbuser')
+db = PostgresqlDatabase(filename="parameter.txt")
 
 
 class BaseModel(Model):
