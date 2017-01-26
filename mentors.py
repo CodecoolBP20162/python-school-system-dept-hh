2b0ecbed6b2e8ed913c764039229ecf93a9c7b9d
anarch1986 <varga.tamas19860808@gmail.com>
2017-01-26 11:53:03 +0100

    mentors.py Mentor object check Interview function defined

--- mentors.py
+++ mentors.py
@@ -1,34 +1,30 @@
 from models import *
 
-class Mentors:
 
+class Mentors:
     @staticmethod
     def check_mentors_interviews(name_input):
 
         interview_list = []
 
-        mentors_interview = (Interview
-                   .select()
-                    .join(InterviewSlot, JOIN.LEFT_OUTER)
-                   .where(InterviewSlot.mentor_id == name_input))
-        print(mentors_interview)
-        for n in mentors_interview:
-            print(interview_list.append([n.start,n.end,n.reserver]))
+        mentors_interview = Interview.select()
 
+        for interview in mentors_interview:
+            if interview.interviewslot.mentor.name==name_input:
+                interview_list.append(interview)
 
         print("**************YOUR INTERVIEWS**************\n")
 
         for interviews in interview_list:
+            print("Your interview starts: {start}\n".format(start=interviews.interviewslot.start))
+            print("Your interview ends: {end}\n".format(end=interviews.interviewslot.end))
+            print("Reserved: {is_reserved}\n".format(is_reserved=interviews.interviewslot.reserved))
+            print("Reserved: {applicant}\n".format(applicant=interviews.applicant.name))
+            print("Applicant's code: {code}\n".format(code=interviews.applicant.code))
 
-            print("Your interview starts: {start}\n".format(start = interviews[0]))
-            print("Your interview ends: {end}\n".format(end=interviews[1]))
-            print("Reserved: {is_reserved}\n".format(is_reserved=interviews[2]))
 
-
-            print("_______________________" + "_" * len(str(interviews[0])))
 
         print("*******************************************\n")
 
         if len(interview_list) == 0:
             print("YOU DON'T HAVE ANY INTERVIEWS.")
-
