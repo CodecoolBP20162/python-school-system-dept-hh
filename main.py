2fde06e5b1abdcba46b57b04a9ab673e557d03f9
Száraz Dénes <szarazdenes@gmail.com>
2017-02-09 14:32:36 +0100

    ui refactored to OOP class method way

--- main.py
+++ main.py
@@ -2,5 +2,22 @@ from ui import Menu
 from administrator import AdministratorData
 
 # Write here your console application
-ui = Menu()
-ui.interface_flow()
+class Main:
+
+    ui = None
+
+    @classmethod
+    def initialize_menu(cls):
+        cls.ui = Menu()
+
+
+    @classmethod
+    def run(cls):
+        if cls.ui is None:
+            cls.initialize_menu()
+        cls.ui.interface_flow()
+
+
+Main.run()
+
+
